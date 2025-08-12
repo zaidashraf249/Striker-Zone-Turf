283c0a3cc32c5d6c33e67906772fbbd211135af2
zaidashraf249 <zaidashraf249@gmail.com>
2025-08-12 21:14:43 +0530

    some changes in index.js

--- Backend/index.js
+++ Backend/index.js
@@ -2,11 +2,9 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./dbConnect.js";
-
-import authRoutes from "./routes/authRoutes.js";
 import contactRouter from "./routes/contactRouter.js";
 import bookingRouter from "./routes/bookingRouter.js";
-import dashboardRoutes from "./routes/dashboardRoutes.js";
+import loginRouter from "./routes/loginRouter.js";
 
 dotenv.config();
 const app = express();
@@ -15,14 +13,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-connectDB();
+connectDB(); // MongoDB connection
 
-// Routes
-app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1", contactRouter);
-app.use("/api/v1", bookingRouter);
-app.use("/api/v1", dashboardRoutes);
+app.use("/api/v1", bookingRouter); 
+app.use("/api/v1", loginRouter); 
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
